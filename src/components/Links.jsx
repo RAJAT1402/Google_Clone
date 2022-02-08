f877a9339c2c332a400db23ae6b0861ef919334a
Rajat Guptat <rajatgupta.tech@gmail.com>
2022-02-08 23:39:59 +0530

    Added effect to active link

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const links = [
@@ -7,12 +7,12 @@ const links = [
     { url: '/images', text: '📸 Images' },
     { url: '/videos', text: '📺 Videos' },
   ];
-
-export const Links = () => {
+  export const Links = () => {
+  const [selected, setSelected] = useState('/search');
   return (
     <div className='flex sm:justify-around justify-between items-center mt-4'>
         {links.map(({url, text}) =>(
-            <NavLink to={url} className="m-2 mb-0">
+            <NavLink to={url} className={(selected == url) ? "text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2" : "m-2 mb-0"} onClick={()=>{setSelected(url)}}>
                 {text}
             </NavLink>
         ))}
